a38a36c90c7b4bc0deda8ee4e0d781ce964c030f
Mike Shultz <shultzm@gmail.com>
2017-07-05 15:17:39 -0600

    Iniitial commit

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var minifyCSS = require('gulp-css');
+var sourcemaps = require('gulp-sourcemaps');
+var babel = require('gulp-babel');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+
+gulp.task('css', function() {
+  return gulp.src('ethmsg/web/static/src/sass/ethmsg.scss')
+    .pipe(sass({
+      includePaths: ['node_modules']
+    }))
+    .pipe(minifyCSS())
+    .pipe(gulp.dest('ethmsg/web/static/dist/css'))
+});
+
+gulp.task('deps', () => {
+  return gulp.src([
+    'node_modules/web3/dist/web3.min.js',
+    'node_modules/mustache/mustache.js',
+    ])
+    .pipe(gulp.dest('etherep/dist/js'));
+});
+
+gulp.task('js', () => {
+  return gulp.src([
+    'node_modules/page/page.js',
+    'etherep/src/js/*.js'
+    ])
+    .pipe(sourcemaps.init())
+    .pipe(babel({
+        presets: ['es2015']
+    }))
+    .pipe(concat('etherep.js'))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('etherep/dist/js'));
+});
+
+gulp.task('default', [ 'css', 'deps', 'js' ]);

c84d698c5fab0771433d67ea2689beeb68c8e552
Mike Shultz <shultzm@gmail.com>
2017-07-10 23:53:58 -0600

    Fleshed out rate, lookup, and refactored _web3Valid for read-only mode and to be more performant

--- etherep/src/js/etherep.js
+++ etherep/src/js/etherep.js
@@ -1,13 +1,18 @@
 ///
 /// CONSTANTS
 ///
-const DEFAULT_PROVIDER = "http://localhost:8545";
-const NETWORKID = 9999;
+const DEFAULT_PROVIDER = "https://ropsten.infura.io/FDJajyHvjza2cZK85SZ6 ";
+const NETWORKS = ["3","9999"];
 const ETHEREP_FEE = 200000000000000;
-const ETHEREP_ADDR = 0x7f8c6b6688fc58e0d5ee4f216c4a3bb900000000;
-const ETHEREP_ABI = [{"constant":false,"inputs":[{"name":"d","type":"bool"}],"name":"setDebug","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[],"name":"drain","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"who","type":"address"}],"name":"setManager","outputs":[{"name":"manager","type":"address"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"who","type":"address"}],"name":"getScore","outputs":[{"name":"score","type":"int256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"who","type":"address"},{"name":"rating","type":"int256"}],"name":"rate","outputs":[],"payable":true,"type":"function"},{"inputs":[{"name":"_manager","type":"address"},{"name":"_fee","type":"uint256"},{"name":"_storageAddress","type":"address"},{"name":"_wait","type":"uint256"}],"payable":false,"type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"name":"sender","type":"address"},{"indexed":false,"name":"message","type":"string"}],"name":"Error","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"message","type":"string"}],"name":"Debug","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"message","type":"int256"}],"name":"DebugInt","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"message","type":"uint256"}],"name":"DebugUint","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"by","type":"address"},{"indexed":false,"name":"who","type":"address"},{"indexed":false,"name":"rating","type":"int256"}],"name":"Rating","type":"event"}];
-const RATINGSTORE_ADDR = null;
+const ETHEREP_ADDR = "0xee7a7b5a928a87f4eefdb1cf9ec9f61287b83f5b";
+const ETHEREP_ABI = [{"constant":false,"inputs":[{"name":"d","type":"bool"}],"name":"setDebug","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[],"name":"drain","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"who","type":"address"}],"name":"getScoreAndCount","outputs":[{"name":"score","type":"int256"},{"name":"ratings","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"who","type":"address"}],"name":"setManager","outputs":[{"name":"manager","type":"address"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"who","type":"address"}],"name":"getScore","outputs":[{"name":"score","type":"int256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"who","type":"address"},{"name":"rating","type":"int256"}],"name":"rate","outputs":[],"payable":true,"type":"function"},{"inputs":[{"name":"_manager","type":"address"},{"name":"_fee","type":"uint256"},{"name":"_storageAddress","type":"address"},{"name":"_wait","type":"uint256"}],"payable":false,"type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"name":"sender","type":"address"},{"indexed":false,"name":"message","type":"string"}],"name":"Error","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"message","type":"string"}],"name":"Debug","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"message","type":"int256"}],"name":"DebugInt","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"message","type":"uint256"}],"name":"DebugUint","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"by","type":"address"},{"indexed":false,"name":"who","type":"address"},{"indexed":false,"name":"rating","type":"int256"}],"name":"Rating","type":"event"}];
+const RATINGSTORE_ADDR = "0x70b3faf62b6d4a3e3d4a61d9f36d43232f4d1cec";
 const RATINGSTORE_ABI = [{"constant":false,"inputs":[{"name":"_debug","type":"bool"}],"name":"setDebug","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"getController","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"target","type":"address"},{"name":"wScore","type":"int256"}],"name":"add","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"target","type":"address"}],"name":"reset","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"target","type":"address"},{"name":"cumulative","type":"int256"},{"name":"total","type":"uint256"}],"name":"set","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"newController","type":"address"}],"name":"setController","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"target","type":"address"}],"name":"get","outputs":[{"name":"","type":"int256"},{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"newManager","type":"address"}],"name":"setManager","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"getDebug","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"getManager","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"inputs":[{"name":"_manager","type":"address"},{"name":"_controller","type":"address"}],"payable":false,"type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"name":"message","type":"string"}],"name":"Debug","type":"event"}];
+const MIGRATION_ADDR = "0x55646c2d031c6be279d34b56a050b38543c302d1";
+const MIGRATION_ABI = [{"constant":true,"inputs":[{"name":"who","type":"address"}],"name":"getPermissions","outputs":[{"name":"","type":"bool"},{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"adminAddress","type":"address"}],"name":"removeAdmin","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"manAddress","type":"address"}],"name":"addManager","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"key","type":"string"}],"name":"getContract","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"key","type":"string"},{"name":"contractAddress","type":"address"}],"name":"setContract","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"adminAddress","type":"address"}],"name":"addAdmin","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"manAddress","type":"address"}],"name":"removeManager","outputs":[],"payable":false,"type":"function"},{"inputs":[{"name":"originalAdmin","type":"address"}],"payable":false,"type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"name":"by","type":"address"},{"indexed":false,"name":"key","type":"string"},{"indexed":false,"name":"contractAddress","type":"address"}],"name":"EventSetContract","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"by","type":"address"},{"indexed":false,"name":"admin","type":"address"}],"name":"EventAddAdmin","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"by","type":"address"},{"indexed":false,"name":"admin","type":"address"}],"name":"EventRemoveAdmin","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"by","type":"address"},{"indexed":false,"name":"manager","type":"address"}],"name":"EventAddManager","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"by","type":"address"},{"indexed":false,"name":"manager","type":"address"}],"name":"EventRemoveManager","type":"event"}];
+
+// Approximate gas to rate
+const APPROX_GAS_RATE = 23000;
 
 ///
 /// UTILS
@@ -17,11 +22,15 @@ function byid(id) {
 }
 
 function submitLookup(form) {
-    console.log('lookup!');
+    if (typeof etherep === 'undefined') console.error("etherep not defined!");
+    else etherep.lookup(form.elements['address'].value);
 }
 
 function selectRating(rating) {
+    console.log("selectRating");
     console.log(rating);
+    var who = byid('rating-address').value;
+    etherep.rate(who, rating);
     return false;
 }
 
@@ -60,18 +69,29 @@ class Etherep {
 
         this.mainElement = el;
 
-        // Get/set web3
-        if (typeof web3 == typeof undefined) {
-            this.web3 = new Web3(new Web3.providers.HttpProvider(DEFAULT_PROVIDER));
-        } else {
-            this.web3 = web3;
-        }
+        this.web3 = null;
+        this.web3Connected = false;
+        this.address = null;
 
         /*if (typeof page == typeof undefined) {
             console.error("page is unavailable, this app will not work properly!");
         }*/
 
-        this._renderMain(byid('tmplMain').innerHTML, { fee: this.web3.fromWei(ETHEREP_FEE, "ether") });
+        this._renderMain(byid('tmplMain').innerHTML, { fee: ETHEREP_FEE / 1000000000000000000 });
+
+        // TBD later
+        this.readOnly = false;
+
+        // Connect
+        if (!this.web3) {
+            if (typeof web3 == typeof undefined) {
+                console.warn("Attempting to create a connection to default RPC");
+                this.web3 = new Web3(new Web3.providers.HttpProvider(DEFAULT_PROVIDER));
+                this.readOnly = true;
+            } else {
+                this.web3 = web3;
+            }
+        }
 
     }
 
@@ -83,46 +103,88 @@ class Etherep {
     _web3Valid(ignoreDefault=false) {
         var that = this;
         return new Promise((resolve, reject) => {
+
             // Are we connected?
             if (that.web3.isConnected()) {
+
+                // Don't do this repeatedly
+                if (that.web3Connected) {
+                    resolve("Already connected");
+                }
+
+                else {
                 
-                // Check network ID
-                that.web3.version.getNetwork((err, netId) =>  {
-                    
-                    // If this failed, maybe we aren't really connected?
-                    if (err) {
-                        reject("User is not using a Web3 browser!");
-                    }
-
-                    // Need to be connected to a specific network
-                    if (netId != NETWORKID) {
-                        reject("User is connected to network " + netId);
-                    } else {
+                    // Check network ID
+                    that.web3.version.getNetwork((err, netId) =>  {
                         
-                        // Sometimes we may need to ignore this check to prevent
-                        // an infinite loop of checks
-                        if (ignoreDefault) {
-                            resolve("Connected");
-                        } else {
-                            
-                            // Check for a default account
-                            if (typeof web3.eth.defaultAccount === "undefined") {
-                                
-                                // We're going to need to wait on this
-                                this.EventAccountSet.add(resolve);
-
-                                // Prompt the user to set the account
-                                this.promptSetAccount();
-
-                            } else {
-                                
-                                resolve("Connected to the proper network");
-
-                            }
+                        // If this failed, maybe we aren't really connected?
+                        if (err) {
+                            reject("User is not using a Web3 browser!");
                         }
-                    }
 
-                });
+                        // Need to be connected to a network with our contracts deployed
+                        if (NETWORKS.includes(netId)) {
+
+                            // Get the migration contract
+                            let migrate = that.web3.eth.contract(MIGRATION_ABI).at(MIGRATION_ADDR);
+
+                            // Get the contract address from the migration contract
+                            migrate.getContract("etherep", function(err, res) {
+
+                                if (err) {
+                                    console.error(err);
+                                    reject('Could not get etherep contract address!');
+                                }
+
+                                else {
+                                
+                                    console.log("Got contract address: ", res);
+                                    that.address = res;
+
+                                    // If we're read-only, we're done
+                                    if (that.readOnly) {
+                                        that.web3Connected = true;
+                                        resolve("Connected read-only");
+                                    } 
+
+                                    // Otherwise, do basic account setup
+                                    else if (!ignoreDefault) {
+                                            
+                                        // Check for a default account
+                                        if (typeof that.web3.eth.defaultAccount === "undefined") {
+                                            
+                                            console.error("Not Implemented: Need to set account");
+                                            that.web3.eth.defaultAccount = that.web3.eth.accounts[0];
+                                            that.web3Connected = true;
+                                            resolve("Connected");
+
+                                        } else {
+                                            
+                                            that.web3Connected = true;
+                                            resolve("Connected to the proper network");
+
+                                        }
+                                    }
+
+                                    // We're still good to go
+                                    else {
+                                        that.web3Connected = true;
+                                        resolve("Connected");
+                                    }
+
+                                }
+
+                            });
+                        } 
+
+                        // Not a valid network
+                        else {
+                            reject("User is connected to network " + netId);
+                        } 
+
+                    });
+
+                }
 
             } else {
                 
@@ -246,7 +308,38 @@ class Etherep {
      * @param {string} address - The ethereum address to lookup
      * @returns {int} - The rating for the address
      */
-    lookup(address) {}
+    lookup(address) {
+        console.log(address);
+        if (!StringValidator.isAddress(address)) {
+            console.error("Invalid address");
+            return;
+        }
+
+        var that = this;
+        this._web3Valid().then(function(res) {
+
+            let targetElement = byid('lookup-score');
+
+            targetElement.classList.remove('hide');
+            targetElement.classList.add('spinner');
+            
+            let rep = that.web3.eth.contract(ETHEREP_ABI).at(that.address);
+            
+            rep.getScoreAndCount.call(address, function(err, resp) {
+                
+                that._render(targetElement, byid('tmplLookupResults').innerHTML, {
+                    address: address,
+                    score: resp[0],
+                    count: resp[1]
+                });
+
+                targetElement.classList.remove('spinner');
+
+            });
+
+        });
+
+    }
 
     /**
      * Rate an address
@@ -268,12 +361,39 @@ class Etherep {
 
         var response;
 
+        var that = this;
         this._web3Valid(true).then(function(res) {
             
-            let rep = this.web3.eth.contract(ETHEREP_ABI).at(ETHEREP_ADDR);
+            let rep = that.web3.eth.contract(ETHEREP_ABI).at(that.address);
 
+            // Get the target element and show loading effect
+            let targetElement = byid('rating-results');
+            targetElement.classList.remove('hide');
+            targetElement.classList.add('spinner');
+            console.log("making rep.rate call");
+            response = rep.rate(address, rating, { value: ETHEREP_FEE, gas: 300000 }, function(err, resp) {
+                console.log('rep.rate callback');
+                console.log(resp);
+                if (err) {
+                    
+                    console.error(err);
+                    return;
 
-            response = rep.rate(address, rating);
+                } else {
+
+                    // Render the notice
+                    that._render(targetElement, byid('tmplRatingResults').innerHTML, {
+                        address: address,
+                        score: rating,
+                        transaction: resp
+                    });
+
+                    // Done loading
+                    targetElement.classList.remove('spinner');
+
+                }
+
+            });
 
         });
 
